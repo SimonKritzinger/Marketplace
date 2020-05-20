a3b866cc7d3c556822b9379d165c27f7651f5c98
Kubsti <64650408+Kubsti@users.noreply.github.com>
2020-05-20 21:45:08 +0200

    Test Push
    Test Push

--- marketplace.bz/inc/js/Scripts.js
+++ marketplace.bz/inc/js/Scripts.js
@@ -1,55 +1,78 @@
+
 function Register(){
 
-    var email = document.getElementById("remailid").value;
-    var pass= document.getElementById("rpasswordid").value;
-    var name= document.getElementById("rnameid").value;
-    var username= document.getElementById("runameid").value;
-    //check that email an password are not null
-    if(email=="" || pass=="" || name=="" || username=="" ){
+  var email = document.getElementById("remailid").value;
+  var pass= document.getElementById("rpasswordid").value;
+  var name= document.getElementById("rnameid").value;
+  var username= document.getElementById("runameid").value;
+  //check that email an password are not null
+  if(email=="" || pass=="" || name=="" || username=="" ){
 
-      $("#Registeremailalert").show();
+    $("#Registeremailalert").show();
 
-    }else{
+  }else{
+    
+    $.ajax
+    ({
+      type:'POST',
+      url:'inc/classes/registeruser.php',
+      data:{
+      email:email,
+      password:pass,
+      name:name,
+      username:username,
+      },
+  
+      success:function(data){
+         if(data){
+          var result = JSON.parse(data);
+          alert(result.message);
 
-      $.ajax
-      ({
-        type:'POST',
-        url:'inc/classes/registeruser.php',
-        data:{
-        email:email,
-        password:pass,
-        name:name,
-        username:username,
-        },
-
-        success:function(data){
-           if(data){
-            var result = JSON.parse(data);
-            alert(result.message);
-
-            if(result.message =="Registration complete"){
-               window.location.href="index.php";
-            }
-           }
-
-     }
-      });
-    }
-
-    return false;
+          if(result.message =="Registration complete"){
+             window.location.href="index.php"; 
+          }
+         }
+      }
+    });
+  }
 }
 
-/*
-function GetAllPosts(){
-  $.ajax({
-    type:'GET',
-    url:'inc/classes/PostDB.php',
+function Login(){
+
+var email = document.getElementById("lemailid").value;
+var pass= document.getElementById("lpasswordid").value;
+
+
+if(pass=="" || email=="" ){
+
+  $("#Registeremailalert").show();
+
+}else{
+  
+  $.ajax
+  ({
+    type:'POST',
+    url:'inc/classes/Login.php',
     data:{
-
+    email:email,
+    password:pass,
     },
-    success:function(data){
 
+    success:function(data){
+       if(data){
+        var result = JSON.parse(data);
+        alert(result.message);
+
+        if(result.message =="Login completed"){
+          $('#LoginForm').modal('hide');
+          var x = document.getElementById("lbutton").style.display='none';
+          var x = document.getElementById("rbutton").style.display='none';
+          
+
+        }
+       }
     }
   });
 }
-*/
+}
+
