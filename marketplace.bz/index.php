09f8bb4eb0de4a85a508ec0d09adc88ac336c777
Lukas <toninilukas@yahoo.de>
2020-06-22 18:03:37 +0200

    Removed pure css styesheet link

--- marketplace.bz/index.php
+++ marketplace.bz/index.php
@@ -9,7 +9,6 @@
 
 		<title>Marketplace</title>
 
-		<link rel="stylesheet" href="https://unpkg.com/purecss@1.0.1/build/pure-min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
